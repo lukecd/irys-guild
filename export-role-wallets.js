97586237248022abfebdd0968f004b87525ef07d
Luke Cassady-Dorion <luke@SPStories.com>
2023-11-06 15:21:42 +0100

    updated code

--- export-role-wallets.js
+++ export-role-wallets.js
@@ -32,15 +32,46 @@ const members = await client.getMembers(
 	guildID,
 	signerFunction, // Optional, if a valid signer is provided, the result will contain private data
 );
+// 1. Create hardcoded variable for the XP of each Role.
+const roleXP = {
+	89687: 42,
+	90844: 20,
+	90898: 36,
+};
 
-// console.log("members=", members);
+// 2. Print each Role and the XP it's worth.
+console.log(`Roles and XP`);
 roles.forEach((role) => {
-	const memberObj = members.find((m) => m.roleId === role.id);
-	console.log(`${role.id} - ${role.name}`);
-	if (memberObj && memberObj.members.length) {
-		memberObj.members.forEach((member) => {
-			console.log(`- ${member}`);
-		});
-	}
-	console.log("\n"); // For spacing between roles
+	console.log(`${role.id} - ${role.name} - ${roleXP[role.id]} XP`);
+});
+console.log("\n");
+// 3. Calculate the XP each wallet address has.
+let walletXP = {};
+members.forEach((memberObj) => {
+	memberObj.members.forEach((wallet) => {
+		if (!walletXP[wallet]) {
+			walletXP[wallet] = 0;
+		}
+		walletXP[wallet] += roleXP[memberObj.roleId];
+	});
+});
+
+// 4. Print wallet addresses under each Role, sorted by XP.
+roles.forEach((role) => {
+	console.log(`${role.id} - ${role.name} - ${roleXP[role.id]} XP`);
+
+	const sortedMembers = members.find((m) => m.roleId === role.id).members.sort((a, b) => walletXP[b] - walletXP[a]);
+
+	sortedMembers.forEach((wallet) => {
+		console.log(`${wallet} - ${walletXP[wallet]} TOTAL XP`);
+	});
+
+	console.log("\n");
+});
+
+// 5. Print a leaderboard of all wallet addresses sorted by XP.
+console.log("LEADERBOARD");
+const sortedWallets = Object.keys(walletXP).sort((a, b) => walletXP[b] - walletXP[a]);
+sortedWallets.forEach((wallet) => {
+	console.log(`${wallet} - ${walletXP[wallet]} XP`);
 });
